c3e5d57c74f16f57d10e9f9227a5336ddedac0ac
Mauro Gestoso <maurogestoso@gmail.com>
2020-05-26 18:38:40 +0100

    Implement NewSSMSourceWithSubstitutions

--- ssm.go
+++ ssm.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"fmt"
+	"regexp"
+	"strings"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ssm"
@@ -34,4 +38,53 @@ func getFromSSM(svc ssmiface.SSMAPI) func(key string) (string, error) {
 		}
 		return *out.Parameter.Value, nil
 	}
+}
+
+// NewSSMSourceWithSubstitutions does stuff
+// TODO temporary API
+func NewSSMSourceWithSubstitutions(subs map[string]string) Source {
+	svc := ssm.New(session.New())
+	return NewSSMSourceWithSubstitutionsWithClient(svc, subs)
+}
+
+// NewSSMSourceWithSubstitutionsWithClient does stuff
+// TODO temporary API
+// I don't like the name of this function. Maybe we could make the svc part of the source struct and make getFromSSMWithSubstitutions a method?
+func NewSSMSourceWithSubstitutionsWithClient(svc ssmiface.SSMAPI, subs map[string]string) Source {
+	return &source{
+		tag: SSMTag,
+		get: getFromSSMWithSubstitutions(svc, subs),
+	}
+}
+
+func getFromSSMWithSubstitutions(svc ssmiface.SSMAPI, subs map[string]string) func(key string) (string, error) {
+	return func(key string) (string, error) {
+		name, err := getParamName(key, subs)
+		if err != nil {
+			return "", err
+		}
+		out, err := svc.GetParameter(&ssm.GetParameterInput{
+			Name: aws.String(name),
+		})
+		if err != nil {
+			return "", err
+		}
+		return *out.Parameter.Value, nil
+	}
+}
+
+func getParamName(source string, subs map[string]string) (result string, err error) {
+	paramsCount := strings.Count(source, "$")
+	re := regexp.MustCompile("\\$\\w+")
+	params := re.FindAll([]byte(source), paramsCount)
+	result = source
+	for _, param := range params {
+		name := string(param[1:])
+		val, found := subs[name]
+		if !found {
+			return "", fmt.Errorf("failed")
+		}
+		result = strings.Replace(result, string(param), val, 1)
+	}
+	return
 }
--- ssm_test.go
+++ ssm_test.go
@@ -40,6 +40,41 @@ func TestSSMSource(t *testing.T) {
 	}
 }
 
+func TestSSMSourceWithSubstitutions(t *testing.T) {
+	testParameterValue := "parameter_value"
+	client := mockSSM{
+		getParameter: func(in *ssm.GetParameterInput) (out *ssm.GetParameterOutput, err error) {
+			if *in.Name != "project/prod/parameter" {
+				t.Fatalf("expected parameter name to be %s, got %s", "project/prod/parameter", *in.Name)
+			}
+
+			out = &ssm.GetParameterOutput{
+				Parameter: &ssm.Parameter{
+					Value: &testParameterValue,
+				},
+			}
+			return
+		},
+	}
+	subs := map[string]string{
+		"stage": "prod",
+	}
+	s := NewSSMSourceWithSubstitutionsWithClient(&client, subs)
+
+	l := NewLoader(s)
+	sett := struct {
+		TestParameter string `ssm:"project/$stage/parameter"`
+	}{}
+
+	err := l.Load(&sett)
+	if err != nil {
+		t.Fatalf("unexpected error %v", err)
+	}
+	if sett.TestParameter != testParameterValue {
+		t.Fatalf("expected value to be %s, got %s", testParameterValue, sett.TestParameter)
+	}
+}
+
 type mockSSM struct {
 	ssmiface.SSMAPI
 	getParameter func(*ssm.GetParameterInput) (out *ssm.GetParameterOutput, err error)
@@ -47,4 +82,50 @@ type mockSSM struct {
 
 func (ssm mockSSM) GetParameter(in *ssm.GetParameterInput) (*ssm.GetParameterOutput, error) {
 	return ssm.getParameter(in)
+}
+
+func Test_getParamName(t *testing.T) {
+	tests := []struct {
+		name    string
+		source  string
+		subs    map[string]string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:   "returns the passed string if it has no parameters",
+			source: "project/prod/ultraSpeed",
+			subs:   map[string]string{},
+			want:   "project/prod/ultraSpeed",
+		},
+		{
+			name:   "replaces all parameters with their corresponding values",
+			source: "project/$stage/$feature",
+			subs: map[string]string{
+				"stage":   "prod",
+				"feature": "ultraSpeed",
+			},
+			want: "project/prod/ultraSpeed",
+		},
+		{
+			name:   "returns an error if it can't find the value for a parameter",
+			source: "project/$stage/$feature",
+			subs: map[string]string{
+				"stage": "prod",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getParamName(tt.source, tt.subs)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getParamName() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("getParamName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
