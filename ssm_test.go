1ced18a8bf70cc4de8405ab023ff0fc5f2d97ef6
Mauro Gestoso <maurogestoso@gmail.com>
2020-05-27 12:30:25 +0100

    [WIP] Add unit test

--- ssm_test.go
+++ ssm_test.go
@@ -117,6 +117,44 @@ func TestSSMSourceWithSubstitutions(t *testing.T) {
 	}
 }
 
+func TestSSMSecureStrings(t *testing.T) {
+	testParameterName := "test/parameter/name"
+	testParameterValue := "parameter_value"
+	sett := struct {
+		TestParameter string `ssm:"test/parameter/name,secure"`
+	}{}
+
+	svc := mockSSM{
+		getParameter: func(in *ssm.GetParameterInput) (out *ssm.GetParameterOutput, err error) {
+			if *in.Name != testParameterName {
+				t.Fatalf("expected parameter name to be %s, got %s", testParameterName, *in.Name)
+			}
+			if in.WithDecryption == nil {
+				t.Fatal("expected input.WithDecryption to be true, got nil")
+			}
+			if *in.WithDecryption == false {
+				t.Fatal("expected input.WithDecryption to be true, got false")
+			}
+			out = &ssm.GetParameterOutput{
+				Parameter: &ssm.Parameter{
+					Value: &testParameterValue,
+				},
+			}
+			return
+		},
+	}
+
+	src := NewSSMSourceWithClient(&svc)
+	l := NewLoader(src)
+	err := l.Load(&sett)
+	if err != nil {
+		t.Fatalf("failed to load config with err: %v", err)
+	}
+	if sett.TestParameter != testParameterValue {
+		t.Fatalf("expected parameter value to be %s, got %s", testParameterValue, sett.TestParameter)
+	}
+}
+
 type mockSSM struct {
 	ssmiface.SSMAPI
 	getParameter func(*ssm.GetParameterInput) (out *ssm.GetParameterOutput, err error)
